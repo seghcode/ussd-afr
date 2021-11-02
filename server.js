8f12aa07c4b3ee61cf336148f762000977790e93
galleh solomon <solomongalleh@gmail.com>
2021-11-02 04:45:50 +0100

    commmit

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Models
-const user = require('./model/user')
+const User = require("./model/user");
 
 // using middleware
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // using morgan
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 // mongoDB connection
 mongoose.connect("mongodb://localhost:27017/ussd", {
   useNewUrlParser: true,
 });
-const db = mongoose.connection
-db.on('error', err => console.log(err))
-db.once('connected', () => console.log('Database running'))
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("connected", () => console.log("Database running"));
 
 // ::::::
-app.get('/', (req, res) => {
-  res.send('Success')
-})
-app.post('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Success");
+});
+app.post("/", (req, res) => {
   // console.log(req.body);
   const {
     phoneNumber,
@@ -38,19 +40,20 @@ app.post('/', (req, res) => {
   } = req.body;
   let response;
 
-  if (text === '') {
-    response = 'CON Enter your full name'
+  if (text === "") {
+    response = "CON Enter your full name";
   }
-  if (text !== '') {
-    let array = text.split('*')
+  if (text !== "") {
+    let array = text.split("*");
     if (array.length === 1) {
-      response = 'CON Enter your id number'
+      response = "CON Enter your id number";
     } else if (array.length === 2) {
       // ID NUMBER
       if (parseInt(array[1]) > 0) {
-        response = 'CON Please confirm if you want to save your data \n1. Confirm \n2. Cancel'
+        response =
+          "CON Please confirm if you want to save your data\n1. Confirm\n2. Cancel";
       } else {
-        response = 'END Network error, Please try again'
+        response = "END Network error, Please try again";
       }
     } else if (array.length === 3) {
       if (parseInt(array[2]) === 1) {
@@ -59,25 +62,23 @@ app.post('/', (req, res) => {
         data.id_number = array[1];
 
         data.save(() => {
-          response = 'END Your data was saved successfully'
-        })
+          response = "END Your data was saved successfully";
+        });
       } else if (parseInt(array[2]) === 2) {
-        response = 'END sorry, Data not saved.'
+        response = "END sorry, Data not saved.";
       } else {
-        response = 'END invalid input.'
+        response = "END invalid input.";
       }
     } else {
-      response = 'END Network error, Please try again'
+      response = "END Network error, Please try again";
     }
   }
 
   setTimeout(() => {
     console.log(text);
     res.send(response);
-    res.end()
+    res.end();
   }, 2000);
-})
+});
 
-
-
-app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
